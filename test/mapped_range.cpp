8bf7ef320677ba47e7d1e209f45de8b911ccc2b7
Gregory Meyer <gregjm@umich.edu>
2018-07-12 11:50:58 -0400

    add apply tests to MappedRangeTest

--- test/mapped_range.cpp
+++ test/mapped_range.cpp
@@ -33,3 +33,53 @@ TEST(MappedRangeTest, MemberFunction) {
     EXPECT_TRUE(std::equal(u.cbegin(), u.cend(), OUTPUT.cbegin())
                 && u.size() == OUTPUT.size());
 }
+
+TEST(MappedRangeTest, Apply) {
+    static constexpr std::array<std::pair<int, int>, 3> INPUT{ {
+        { 0, 1 },
+        { 2, 3 },
+        { 4, 5 }
+    } };
+    static constexpr std::array<int, 3> OUTPUT{ { 1, 5, 9 } };
+
+    const std::vector<int> v =
+        umigv::ranges::map(INPUT, [](int x, int y) { return x + y; }).collect();
+
+    EXPECT_TRUE(std::equal(v.cbegin(), v.cend(), OUTPUT.cbegin())
+                && v.size() == OUTPUT.size());
+}
+
+TEST(MappedRangeTest, ApplyOverload) {
+    struct Adder {
+        constexpr int operator()(int x, int y) noexcept {
+            ++apply_count;
+
+            return x + y;
+        }
+
+        constexpr int operator()(const std::pair<int, int> &pair) noexcept {
+            ++invoke_count;
+
+            return pair.first + pair.second;
+        }
+
+        int apply_count = 0;
+        int invoke_count = 0;
+    };
+
+    static constexpr std::array<std::pair<int, int>, 3> INPUT{ {
+        { 0, 1 },
+        { 2, 3 },
+        { 4, 5 }
+    } };
+    static constexpr std::array<int, 3> OUTPUT{ { 1, 5, 9 } };
+
+    Adder adder;
+
+    const std::vector<int> v =
+        umigv::ranges::map(INPUT, std::ref(adder)).collect();
+
+    EXPECT_TRUE(std::equal(v.cbegin(), v.cend(), OUTPUT.cbegin())
+                && v.size() == OUTPUT.size()
+                && adder.apply_count == 0 && adder.invoke_count == 3);
+}
