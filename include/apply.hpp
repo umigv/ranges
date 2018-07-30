6b0b8be08fcce82074855d4295a569a7305ebb18
Gregory Meyer <gregjm@umich.edu>
2018-07-30 00:57:32 -0400

    use umigv_ranges_apply_detail namespace

--- include/apply.hpp
+++ include/apply.hpp
@@ -47,7 +47,7 @@ struct is_applicable : std::false_type { };
 
 template <typename C, typename T>
 struct is_applicable<C, T, void_t<std::enable_if_t<is_tuple<T>::value>>>
-: decltype(detail::check_applicable(
+: decltype(umigv_ranges_apply_detail::check_applicable(
     std::declval<C>(),
     std::declval<T>(),
     std::make_index_sequence<tuple_size<T>::value>()
@@ -58,7 +58,7 @@ struct is_nothrow_applicable : std::false_type { };
 
 template <typename C, typename T>
 struct is_nothrow_applicable<C, T, void_t<std::enable_if_t<is_tuple<T>::value>>>
-: decltype(detail::check_nothrow_applicable(
+: decltype(umigv_ranges_apply_detail::check_nothrow_applicable(
     std::declval<C>(),
     std::declval<T>(),
     std::make_index_sequence<tuple_size<T>::value>()
@@ -69,11 +69,13 @@ struct apply_result { };
 
 template <typename C, typename T>
 struct apply_result<C, T, void_t<std::enable_if_t<is_tuple<T>::value>>> {
-    using type = typename decltype(detail::check_apply_result(
-        std::declval<C>(),
-        std::declval<T>(),
-        std::make_index_sequence<tuple_size<T>::value>()
-    ))::type;
+    using type = typename decltype(
+        umigv_ranges_apply_detail::check_apply_result(
+            std::declval<C>(),
+            std::declval<T>(),
+            std::make_index_sequence<tuple_size<T>::value>()
+        )
+    )::type;
 };
 
 template <typename C, typename T>
@@ -85,8 +87,11 @@ template <typename C, typename T,
           > = 0>
 constexpr apply_result_t<C, T> apply(C &&callable, T &&tuple)
 noexcept(is_nothrow_applicable<C, T>::value) {
-    return detail::apply(std::forward<C>(callable), std::forward<T>(tuple),
-                         std::make_index_sequence<tuple_size<T>::value>());
+    return umigv_ranges_apply_detail::apply(
+        std::forward<C>(callable),
+        std::forward<T>(tuple),
+        std::make_index_sequence<tuple_size<T>::value>()
+    );
 }
 
 } // namespace ranges
