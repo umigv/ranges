ffda757622763de5f7ae50540f2a474e8e361730
Gregory Meyer <gregjm@umich.edu>
2018-07-31 01:05:28 -0400

    use CountIter for CountingRange

--- include/counting_range.hpp
+++ include/counting_range.hpp
@@ -33,86 +33,10 @@
 #define UMIGV_RANGES_COUNTING_RANGE_HPP
 
 #include "range_fwd.hpp"
-#include "traits.hpp"
-
-#include <cstddef>
-#include <stdexcept>
+#include "count_iter.hpp"
 
 namespace umigv {
 namespace ranges {
-
-template <typename T>
-class CountingRange;
-
-template <typename T>
-class CountingRangeIterator {
-public:
-    using difference_type = std::ptrdiff_t;
-    using iterator_category = std::input_iterator_tag;
-    using pointer = const T*;
-    using reference = const T&;
-    using value_type = T;
-
-    friend CountingRange<T>;
-
-    constexpr reference operator*() const {
-        range_check("CountingRangeIterator::operator*");
-
-        return current_;
-    }
-
-    constexpr pointer operator->() const noexcept {
-        return std::addressof(**this);
-    }
-
-    constexpr CountingRangeIterator& operator++() {
-        range_check("CountingRangeIterator::operator++");
-
-        current_ += step_;
-
-        return *this;
-    }
-
-    constexpr CountingRangeIterator operator++(int) {
-        const auto to_return = *this;
-
-        ++(*this);
-
-        return to_return;
-    }
-
-    friend constexpr bool operator==(const CountingRangeIterator &lhs,
-                                     const CountingRangeIterator &rhs) {
-        if (lhs.step_ != rhs.step_ || lhs.end_ != rhs.end_) {
-            throw std::out_of_range{ "CountingRangeIterator::operator==" };
-        }
-
-        return lhs.current_ == rhs.current_;
-    }
-
-    friend constexpr bool operator!=(const CountingRangeIterator &lhs,
-                                     const CountingRangeIterator &rhs) {
-        return !(lhs == rhs);
-    }
-
-private:
-    constexpr CountingRangeIterator(const T &current, const T &step,
-                                    const T &end) noexcept
-    : current_{ current }, step_{ step }, end_{ end } { }
-
-    template <std::size_t N>
-    constexpr void range_check(const char (&what)[N]) const {
-        if (step_ > 0 && current_ >= end_) {
-            throw std::out_of_range{ what };
-        } else if (step_ < 0 && current_ <= end_) {
-            throw std::out_of_range{ what };
-        }
-    }
-
-    T current_ = 0;
-    T step_ = 1;
-    T end_;
-};
 
 template <typename T>
 class CountingRange : public Range<CountingRange<T>> {
@@ -124,28 +48,28 @@ public:
     using reference = typename RangeTraits<CountingRange>::reference;
     using value_type = typename RangeTraits<CountingRange>::value_type;
 
-    constexpr explicit CountingRange(const T &end) noexcept : end_{ end } { }
+    constexpr explicit CountingRange(const T &end) noexcept
+    : CountingRange{ 0, 1, end } { }
 
     constexpr CountingRange(const T &begin, const T &end) noexcept
-    : begin_{ begin }, end_{ end } { }
+    : CountingRange{ begin, 1, end } { }
 
     constexpr CountingRange(const T &begin, const T &step,
                             const T &end) noexcept
-    : begin_{ begin }, step_{ step },
-      end_{ begin + step * (end - begin) / step } { }
+    : first_{ begin, step },
+      last_{ begin + step * (end - begin) / step, step } { }
 
-    constexpr CountingRangeIterator<T> begin() const noexcept {
-        return { begin_, step_, end_ };
+    constexpr iterator begin() const noexcept {
+        return first_;
     }
 
-    constexpr CountingRangeIterator<T> end() const noexcept {
-        return { end_, step_, end_ };
+    constexpr iterator end() const noexcept {
+        return last_;
     }
 
 private:
-    T begin_ = 0;
-    T step_ = 1;
-    T end_;
+    iterator first_;
+    iterator last_;
 };
 
 template <typename T>
@@ -166,11 +90,11 @@ constexpr CountingRange<T> range(const T &begin, const T &step,
 
 template <typename T>
 struct RangeTraits<CountingRange<T>> {
-    using difference_type = iterator_difference_t<CountingRangeIterator<T>>;
-    using iterator = CountingRangeIterator<T>;
-    using pointer = iterator_pointer_t<CountingRangeIterator<T>>;
-    using reference = iterator_reference_t<CountingRangeIterator<T>>;
-    using value_type = iterator_value_t<CountingRangeIterator<T>>;
+    using difference_type = iterator_difference_t<CountIter<T>>;
+    using iterator = CountIter<T>;
+    using pointer = iterator_pointer_t<CountIter<T>>;
+    using reference = iterator_reference_t<CountIter<T>>;
+    using value_type = iterator_value_t<CountIter<T>>;
 };
 
 } // namespace ranges
