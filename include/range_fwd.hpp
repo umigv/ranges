c16a3270187242039a1d711b2aa85a1020fb1c43
Gregory Meyer <gregjm@umich.edu>
2018-08-06 10:26:50 -0400

    move RangeTraits into own module

--- include/range_fwd.hpp
+++ include/range_fwd.hpp
@@ -38,15 +38,6 @@ namespace ranges {
 template <typename R>
 class Range;
 
-template <typename R>
-struct RangeTraits {
-    using difference_type = typename R::difference_type;
-    using iterator = typename R::iterator;
-    using pointer = typename R::pointer;
-    using reference = typename R::reference;
-    using value_type = typename R::value_type;
-};
-
 } // namespace ranges
 } // namespace umigv
 
